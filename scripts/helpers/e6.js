85e92bd93a70ccafb96ea6a2bac5a01ab9d14b73
slysop <xd@d.com>
2014-10-24 16:27:06 +0000

    Inital Working

--- scripts/helpers/e6.js
+++ scripts/helpers/e6.js
@@ -1,24 +1,120 @@
-/* Requirements 
+/* Requirements
 
-+ -- 
++ --
 + require.js
 + // load require.js like so
 + <script data-main="scripts/main" src="scripts/require.js"></script>
 + --
-+ 
++
 require(["helper/e6.js"], function(e6){
   e6.init();
 });
 */
 
-define(function(){
-  return {
+var obj = {
     init : function(){
-      this.methods();
+        return Array().slice.call(arguments);
     },
-    methods : function(){
-      console.log('inside methods');
-      return this;
+    methods : {
+        find : function(predicate){
+            console.log('predicate');
+        }
     }
-  }
-});
+};
+
+exports.e6 = function(){
+  return  {
+    init : function(){
+        return Array().slice.call(arguments);
+    },
+    methods : {
+        find : function(predicate){
+            if(!Array.prototype.find){
+                throw new TypeError('Array.prototype.find called on null or undefined');
+            }
+            if(typeof predicate !== 'function'){
+                throw new TypeError('predicate must be a function');
+            }
+            var list = Object(this),
+                length = list.length >>> 0,
+                thisArg = arguments[1],
+                value;
+
+            (function retunFunc(n){
+                var n = n || 0;
+                n >= length ?
+                    (function(){
+                        return false;
+                    })() :
+                    (function(){
+                        n++;
+                        value = list[n];
+                        if(predicate.call(thisArg, value, n, list)){
+                            return value;
+                        }
+                        retunFunc(n);
+                    })();
+            })();
+            return undefined;
+        }
+    }
+ };
+};
+        /*
+        Summary
+        -------
+        The find() method returns a value in the array,
+        if an element in the array satisfies the provided
+        testing function. Otherwise undefined is returned.
+
+        Syntax
+        ------
+        arr.find(callback[, thisArg])
+
+        Parameters
+        ----------
+        callback
+          Function to execute on each value in the array, taking three arguments:
+        element
+          The current element being processed in the array.
+        index
+          The index of the current element being processed in the array.
+        array
+          The array find was called upon.
+        thisArg
+          Optional. Object to use as this when executing callback.
+        
+        methods : {
+            find : function(predicate){
+                if(!Array.prototype.find){
+                    throw new TypeError('Array.prototype.find called on null or undefined');
+                }
+                if(typeof predicate !== 'function'){
+                    throw new TypeError('predicate must be a function');
+                }
+                var list = Object(this),
+                    length = list.length >>> 0,
+                    thisArg = arguments[1],
+                    value;
+
+                (function retunFunc(n=0){
+                    n >= length ?
+                        (function(){
+                            return false;
+                        })() :
+                        (function(){
+                            n++;
+                            value = list[n];
+                            if(predicate.call(thisArg, value, n, list)){
+                                return value;
+                            }
+                            retunFunc(n);
+                        })();
+                })();
+                return undefined;
+            }
+        }
+}
+
+
+*/
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
-require(["scripts/helpers/e6.js"], function(e6){
-  e6.init();
-});
+'use strict';
+
+(function(){
+    var mod = require('module');
+    console.log(mod.greet());
+    
+    /* 
+    require our e6 library 
+    */
+    var edMod = require('e6');
+    //Create new object from module
+    var _e6 = Object.create(edMod.e6());
+    //call the init method
+    console.log(_e6.init(1,2,3));
+})();
+
+
+
