c6b357f0162a9ccf10a603a778e798c6054c7bb4
slysop <xd@d.com>
2014-10-30 20:26:26 +0000

    Array.of added to lib plus test

--- scripts/helpers/e6.js
+++ scripts/helpers/e6.js
@@ -59,65 +59,16 @@ exports.e6 = function(){
                 };
             }
             
+            /* Array of 
+              Returns an array of the elements provided
+             */
+             if(typeof Array.of !== 'function'){
+                 Array.of = function(){
+                     return Array.prototype.slice.call(arguments)
+                 }
+             }
+            
         }
     }
  };
 };
-        /*
-        Summary
-        -------
-        The find() method returns a value in the array,
-        if an element in the array satisfies the provided
-        testing function. Otherwise undefined is returned.
-
-        Syntax
-        ------
-        arr.find(callback[, thisArg])
-
-        Parameters
-        ----------
-        callback
-          Function to execute on each value in the array, taking three arguments:
-        element
-          The current element being processed in the array.
-        index
-          The index of the current element being processed in the array.
-        array
-          The array find was called upon.
-        thisArg
-          Optional. Object to use as this when executing callback.
-        
-        methods : {
-            find : function(predicate){
-                if(!Array.prototype.find){
-                    throw new TypeError('Array.prototype.find called on null or undefined');
-                }
-                if(typeof predicate !== 'function'){
-                    throw new TypeError('predicate must be a function');
-                }
-                var list = Object(this),
-                    length = list.length >>> 0,
-                    thisArg = arguments[1],
-                    value;
-
-                (function retunFunc(n=0){
-                    n >= length ?
-                        (function(){
-                            return false;
-                        })() :
-                        (function(){
-                            n++;
-                            value = list[n];
-                            if(predicate.call(thisArg, value, n, list)){
-                                return value;
-                            }
-                            retunFunc(n);
-                        })();
-                })();
-                return undefined;
-            }
-        }
-}
-
-
-*/
