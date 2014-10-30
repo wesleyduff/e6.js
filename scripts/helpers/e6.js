f01e9c7ee269a2941b45135957e8a1d369a4b2ae
slysop <xd@d.com>
2014-10-30 15:41:39 +0000

    Array - Find

--- scripts/helpers/e6.js
+++ scripts/helpers/e6.js
@@ -11,51 +11,36 @@ require(["helper/e6.js"], function(e6){
 });
 */
 
-var obj = {
-    init : function(){
-        return Array().slice.call(arguments);
-    },
-    methods : {
-        find : function(predicate){
-            console.log('predicate');
-        }
-    }
-};
-
 exports.e6 = function(){
   return  {
     init : function(){
-        return Array().slice.call(arguments);
+        this.methods.buildArrayMethods();
     },
     methods : {
-        find : function(predicate){
+        buildArrayMethods : function(){
             if(!Array.prototype.find){
-                throw new TypeError('Array.prototype.find called on null or undefined');
+                Array.prototype.find = function(predicate) {
+                    if (this == null) {
+                      throw new TypeError('Array.prototype.find called on null or undefined');
+                    }
+                    if (typeof predicate !== 'function') {
+                      throw new TypeError('predicate must be a function');
+                    }
+                    var list = Object(this);
+                    var length = list.length >>> 0;
+                    var thisArg = arguments[1];
+                    var value;
+                
+                    for (var i = 0; i < length; i++) {
+                      value = list[i];
+                      if (predicate.call(thisArg, value, i, list)) {
+                        return value;
+                      }
+                    }
+                    return undefined;
+                  };
             }
-            if(typeof predicate !== 'function'){
-                throw new TypeError('predicate must be a function');
-            }
-            var list = Object(this),
-                length = list.length >>> 0,
-                thisArg = arguments[1],
-                value;
-
-            (function retunFunc(n){
-                var n = n || 0;
-                n >= length ?
-                    (function(){
-                        return false;
-                    })() :
-                    (function(){
-                        n++;
-                        value = list[n];
-                        if(predicate.call(thisArg, value, n, list)){
-                            return value;
-                        }
-                        retunFunc(n);
-                    })();
-            })();
-            return undefined;
+            
         }
     }
  };
